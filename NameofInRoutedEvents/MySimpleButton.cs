218f4ba451aa96b4b459bb8411f78f9d3f6a73b9
Peter Wendorff <jongleur1983@gmx.de>
2016-06-06 21:31:25 +0200

    project to proof that nameof works across static and non-static contexts, something I was not sure about initially and others thought it were not possible.

---
+++ NameofInRoutedEvents/MySimpleButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+/// <summary>
+/// this file is directly taken from the example at https://msdn.microsoft.com/de-de/library/ms752288(v=vs.110).aspx
+/// </summary>
+namespace NameofInRoutedEvents
+{
+    public class MyButtonSimple : Button
+    {
+        public static readonly RoutedEvent TapEvent = EventManager.RegisterRoutedEvent(
+            nameof(Tap),
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(MyButtonSimple));
+
+        public event RoutedEventHandler Tap
+        {
+            add
+            {
+                this.AddHandler(TapEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(TapEvent, value);
+            }
+        }
+
+        void RaiseTapEvent()
+        {
+            var newEventArgs = new RoutedEventArgs(MyButtonSimple.TapEvent);
+            this.RaiseEvent(newEventArgs);
+        }
+
+        protected override void OnClick()
+        {
+            this.RaiseTapEvent();
+        }
+    }
+}
